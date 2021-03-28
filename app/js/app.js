d3e722c0606f38a1ac49fffb76b820635378e3f6
unknown <yanis.lebzar@ecoles-wis.net>
2021-03-28 21:12:42 +0200

    2nd save

---
+++ app/js/app.js
@@ -0,0 +1,30 @@
+// clique sur le burger
+let spanBurger = document.querySelector('.hamburger');
+let menuBurger = document.createElement('div');
+let mask = document.querySelector('#full-size');
+menuBurger.classList.toggle('burger_id');
+menuBurger.id = 'test'
+// barre du milieu qui disparait + barre du haut et du bas qui rotate  
+spanBurger.addEventListener('click', event =>{
+    //document.body.style = "overflow : hidden;"
+spanBurger.classList.toggle('active');
+menuBurger.classList.toggle('activeBurger');
+mask.classList.toggle('mask');
+})
+// div menu sur le coté droit qui arrive
+document.body.appendChild(menuBurger);
+mask.onclick = () => {
+    if(spanBurger.classList.contains('active')){
+      //  document.body.style = "overflow : inherit;"
+
+        spanBurger.classList.remove('active');  
+   menuBurger.classList.remove('activeBurger');
+        mask.classList.remove('mask');
+    }
+
+}
+       
+   
+
+// si on rappuie sur la croix ou sur la page de base, ça s'enleve
+// le burger redevient normal
