db9131921b13ae8665b028ad9db04192cf8df9ea
unknown <yanis.lebzar@ecoles-wis.net>
2021-03-31 18:36:31 +0200

    js sidebar/responsive buttons

--- app/js/app.js
+++ app/js/app.js
@@ -1,34 +1,70 @@
 // clique sur le burger
 let spanBurger = document.querySelector('.hamburger');
 let menuBurger = document.querySelector('.burger_menu');
+
+let menuBurgerM1 = document.querySelector('.burger_menuM1');
+let menuBurgerM2 = document.querySelector('.burger_menuM2');
+let plusButton = document.querySelector('.plusButton');
+let plusButtonRetour = document.querySelector('.plusButtonRetour');
 let mask = document.querySelector('#full-size');
+const mediaQueryNav = window.matchMedia('(max-width: 1100px)');
 // barre du milieu qui disparait + barre du haut et du bas qui rotate  
 
-function openNav(){
+// function web and mobile side bar come
+function openNav() {
     spanBurger.classList.toggle('active');
+    mask.style.height = "100%";
 
-    document.getElementById("menuBurger").style.width = "300px";
-    document.getElementById("menuBurger").style.padding = "35px 20px 20px 20px";
+    if (mediaQueryNav.matches) {
+        document.getElementById("menuBurgerM1").style.width = "300px";
+        document.getElementById("menuBurgerM1").style.padding = "35px 20px 20px 20px";
+    } else {
+        document.getElementById("menuBurger").style.width = "300px";
+        document.getElementById("menuBurger").style.padding = "35px 20px 20px 20px";
+    }
 
-    mask.classList.toggle('mask');
+
 
 }
-function closeNav(){
-    document.getElementById("menuBurger").style.width = "0px";
-    document.getElementById("menuBurger").style.padding = "55px 0px 20px 0px";
 
-    spanBurger.classList.remove('active');  
-    mask.classList.remove('mask');
+// plus button
+
+plusButton.addEventListener('click', event =>{
+    menuBurgerM2.style.display = 'block';
+})
+plusButtonRetour.addEventListener('click', event =>{
+    menuBurgerM2.style.display = 'none';
+})
+
+// function web and mobile side bar close
+
+function closeNav() {
+    if (mediaQueryNav.matches) {
+
+        document.getElementById("menuBurgerM1").style.width = "0px";
+        document.getElementById("menuBurgerM1").style.padding = "55px 0px 20px 0px";
+    } else {
+        document.getElementById("menuBurger").style.width = "0px";
+        document.getElementById("menuBurger").style.padding = "55px 0px 20px 0px";
+    }
+
+    spanBurger.classList.remove('active');
+    mask.style.height = "0%";
+    menuBurgerM2.style.display = 'none';
+
+
 }
 
 
 
 
 
-spanBurger.addEventListener('click', event =>{
-    if(spanBurger.classList.contains('active')){
+
+
+spanBurger.addEventListener('click', event => {
+    if (spanBurger.classList.contains('active')) {
         closeNav()
-    }else {
+    } else {
         openNav()
 
     }
@@ -37,15 +73,15 @@ spanBurger.addEventListener('click', event =>{
 // div menu sur le coté droit qui arrive
 document.body.appendChild(menuBurger);
 mask.onclick = () => {
-    if(spanBurger.classList.contains('active')){
+    if (spanBurger.classList.contains('active')) {
 
-       return closeNav();
+        return closeNav();
     }
 
 }
 
-       
-   
+
+
 
 // si on rappuie sur la croix ou sur la page de base, ça s'enleve
 // le burger redevient normal
